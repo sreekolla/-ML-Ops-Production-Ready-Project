8148de56737bd677e7f5e25fed79cc5fb2c64252
Sreerasya Kolla <sr.kolla1998@gmail.com>
2025-08-25 13:38:30 -0400

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sreerasya",
+    author_email="sr.kolla1998@gmail.com",
+    packages=find_packages()
+)
