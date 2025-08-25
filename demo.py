8de0f5ee1a1be8eb33e4c38bc4433ae5e6d8d726
Sreerasya Kolla <sr.kolla1998@gmail.com>
2025-08-25 18:24:57 -0400

    logger,exception,utils updated

---
+++ demo.py
@@ -0,0 +1,8 @@
+from us_visa.logger  import logging
+from us_visa.exception import USvisaException
+logging.info("Welcome to our custom log")
+
+try:
+    a= 2/0
+except Exception as e:
+    raise USvisaException(e.sys)
