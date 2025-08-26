76c646d78905d5255d1036393231bfbff922b65c
Sreerasya Kolla <sr.kolla1998@gmail.com>
2025-08-26 19:41:04 -0400

    data ingestion  added

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
-from us_visa.logger  import logging
-from us_visa.exception import USvisaException
-logging.info("Welcome to our custom log")
+from us_visa.pipline.training_pipeline import TrainPipeline
 
-try:
-    a= 2/0
-except Exception as e:
-    raise USvisaException(e.sys)
+obj =TrainPipeline()
+obj.run_pipeline()
